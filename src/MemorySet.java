0f9f0d9c77fc7889684feb3483953de357d8bd45
Sherif Mowafy <sherif.mowafy@gmail.com>
2015-12-04 23:10:14 +0200

    Modified memory hierarchy
    1. Integrated cycle counting and corresponding constructor
    2. Instruction and data separate caches for Level 1 only, the rest is unified

--- src/MemorySet.java
+++ src/MemorySet.java
@@ -2,12 +2,47 @@ import java.util.LinkedList;
 
 
 
-
 public class MemorySet {
-	
+	public static int cycles;
 	
 	private MainMemory mainMemory;
 	private LinkedList<Cache> caches;
+	
+	private Cache instructionCache, dataCache;
+	
+	public MemorySet(int numberOfLevels) {
+		cycles = 0;
+		mainMemory = new MainMemory();
+		caches = new LinkedList<Cache>();
+		int x, y, z, cyc;
+		x =  y = z = cyc = 0;
+		instructionCache = new Cache(x, y, z, cyc);
+		dataCache = new Cache(x, y, z, cyc);
+		for(int i = 0; i < numberOfLevels - 1; i++) {
+			caches.add(new Cache(x, y, z, cyc));
+			if (i > 0) {
+				caches.get(i - 1).setHigherLevel(caches.getLast());
+			} else {
+				instructionCache.setHigherLevel(caches.getLast());
+				dataCache.setHigherLevel(caches.getLast());
+			}
+		}
+		caches.getLast().setMainMemory(mainMemory);
+	}
+	
+	public int[] getBlock(int address) {
+		cycles = 0;
+		int threshold = (1 << 8);
+		if (address >= threshold) {
+			return instructionCache.getBlock(address);
+		} else {
+			return dataCache.getBlock(address);
+		}
+	}
+	
+	public int getNumberOfCyclesSpent() {
+		return cycles;
+	}
 	
 	
 	
