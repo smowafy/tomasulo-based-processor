194833f54c14de25c9599c85fcc786ccbab8c9c9
Ayatallah <ayatallah.amr@gmail.com>
2015-12-06 21:09:08 +0200

    change mainmemo to memoset

--- src/Tomasulo/Processor.java
+++ src/Tomasulo/Processor.java
@@ -1,22 +1,22 @@
 package Tomasulo;
 
 import Buffers.*;
-import MemoryHierarchy.MainMemory;
+import MemoryHierarchy.*;
 import Registers.*;
 import ReservationStation.*;
 
 public class Processor {
 	private int cycles;
 	private int pc;
-	private MainMemory dataMemory;
-	private MainMemory insMemory;
+	private MemorySet dataMemory;
+	private MemorySet insMemory;
 	private InstructionBuffer insBuffer;
 	private ReservationStations reservationStation;
 	private ReorderBuffer reorderBuffer;
 	private Registers registersFile;
 	private RegisterStat registerStat;
 	
-	public Processor(int pc, MainMemory dataMemory,MainMemory insMemory,InstructionBuffer insBuffer,
+	public Processor(int pc, MemorySet dataMemory,MemorySet insMemory,InstructionBuffer insBuffer,
 			ReservationStations reservationStation,ReorderBuffer reorderBuffer,Registers registersFile,
 			RegisterStat registerStat){
 		this.pc = pc;
